c6eda7d530ace13295562290e7fc35569c10322e
m83Dragan <sergioxyzribeiro@gmail.com>
2018-07-19 13:38:45 +0800

    complete

--- src/App.js
+++ src/App.js
@@ -2,18 +2,42 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as counterActions from './modules/counter';
+import * as postActions from './modules/post';
 
 
 class App extends Component {
+    componentDidMount(){
+        const { PostActions, number } = this.props;
+        PostActions.getPost(number);
+    }
+    componentWillReceiveProps(nextProps){
+        const { PostActions } = this.props;
+        if(this.props.number !== nextProps.number){
+            PostActions.getPost(nextProps.number);
+        }
+    }
     render() {
-        const { CounterActions, number } = this.props;
+        const { CounterActions, number, loading, error, data } = this.props;
 
         
         return (
             <div>
-                <h1>{number}</h1>
-                <button onClick={CounterActions.increment}>+</button>
-                <button onClick={CounterActions.decrement}>-</button>
+                <div>
+                    <h1>{number}</h1>
+                    <button onClick={CounterActions.incrementAsync}>+</button>
+                    <button onClick={CounterActions.decrementAsync}>-</button>
+                </div>
+                <div>
+                    {loading && <h1>Loading</h1>}
+                    {error
+                        ? <h1>Error</h1>
+                        : (
+                            <div>
+                                <h1>{data.title}</h1>
+                                <p>{data.body}</p>
+                            </div>
+                        )}
+                </div>
             </div>
         );
     }
@@ -21,9 +45,13 @@ class App extends Component {
 
 export default connect(
     (state) => ({
-        number: state.counter
+        number: state.counter,
+        loading: state.post.pending,
+        error: state.post.error,
+        data: state.post.data
     }),
     (dispatch) => ({
-        CounterActions: bindActionCreators(counterActions, dispatch)
+        CounterActions: bindActionCreators(counterActions, dispatch),
+        PostActions: bindActionCreators(postActions, dispatch)
     })
 )(App);
